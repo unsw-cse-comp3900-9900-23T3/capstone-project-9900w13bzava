1365a6ba6d63e0de7598282aaf2c8b19309c91d0
YL0202 <1520324412lyl@gmail.com>
2023-10-25 00:46:29 +1100

    Update Return Name

--- code_1/backend/backend.py
+++ code_1/backend/backend.py
@@ -137,18 +137,18 @@ def register():
 
 # 根据输入的userid和date来获取query
 def get_query(userid, date):
-    return f'''SELECT table1.RECORDID, table1.APPOINTMENTDATE, table1.APPOINTMENTLENGTH, table1.APPOINTMENTTIME, table4.DESCRIPTION, table5.DESCRIPTION as Status, table2.firstname, table2.SURNAME, table3.PREFERREDNAME, table3.INTERNALID,
+    return f'''SELECT table1.RECORDID, table1.APPOINTMENTDATE, table1.APPOINTMENTLENGTH as duration, table1.APPOINTMENTTIME as startTime, table4.DESCRIPTION as appointmentType, table5.DESCRIPTION as status, table2.firstname firstName, table2.SURNAME as surname, table3.PREFERREDNAME as patientName, table3.INTERNALID as patientID,
       CASE
         WHEN table1.APPOINTMENTTYPE=30 and table3.INTERNALID in (
 			SELECT INTERNALID FROM BPSSamples.dbo.APPOINTMENTS
 			WHERE LOCATIONID=1 and DATEDIFF(DAY, GETDATE(), APPOINTMENTDATE)<= -365
 		) THEN 1
         ELSE 0
-      END AS IsPhone,
+      END AS isPhone,
       CASE
         WHEN table3.MEDICARENO IS NULL THEN 0
         ELSE 1
-      END AS HasMedicare
+      END AS hasMedicare
     FROM BPSSamples.dbo.APPOINTMENTS as table1
     inner join BPSSamples.dbo.USERS as table2
     ON table1.USERID = table2.USERID
@@ -161,6 +161,24 @@ def get_query(userid, date):
     where table1.USERID={userid} and table1.APPOINTMENTDATE=\'{date}\'
   '''
 
+
+def proccess_result(result, dayType):
+    sum = 0
+    # convert second to readable time format
+    for idx in result:
+        sum += int(idx['duration'])
+        sec = int(idx['startTime']) 
+        time = datetime.timedelta(seconds=sec)
+        if int(str(time).split(':')[0]) >= 12:
+           idx['startTime'] = f'{str(time)[:-3]} pm'
+        else:
+           idx['startTime'] = f'{str(time)[:-3]} am'
+        idx['patientName'] = str(idx['patientName']).strip()
+        idx['note'] = "nima si "
+        idx['dayType'] = f"{dayType}"
+        idx['duration'] = str(int(idx['duration'])//60)
+
+    return result, sum
 
 # ShowAppt
 @app.route('/ShowPanel', methods=['POST'])
@@ -203,20 +221,30 @@ def ShowPanel():
     result2 = json.loads(ConnectDatabase.myConnect(query2))
     result3 = json.loads(ConnectDatabase.myConnect(query3))
 
-    sum = 0
-    # convert second to readable time format
-    for idx in result1:
-        sum += int(idx['APPOINTMENTLENGTH'])
-        sec = int(idx['APPOINTMENTTIME'])
-        time = datetime.timedelta(seconds=sec)
-        idx['APPOINTMENTTIME'] = str(time)
+    result1, sum = proccess_result(result1, 1)  # 当天
+    result2, _ = proccess_result(result2, 0)  # 前一天
+    result3, _ = proccess_result(result3, 2)  # 后一天
+
+    # sum = 0
+    # # convert second to readable time format
+    # for idx in result1:
+    #     sum += int(idx['duration'])
+    #     sec = int(idx['startTime'])
+    #     time = datetime.timedelta(seconds=sec)
+    #     idx['startTime'] = str(time)
+    #     idx['patientName'] = str(idx['patientName']).strip()
+    #     idx['note'] = "nima si le"
+
+    #     idx['dayType'] = "1"
     
-    stat = f'''On {date},
+    stat = f'''On {cur_date},
     I have {len(result1)} appointment(s) in total.
     My expected workload duration is {sum/3600:.1f} hour(s).'''
 
-    print(result1)
-    return jsonify(result1, result2, result3, stat)
+    # print(result1)
+    # print(type(result1), type(result1[0]))
+    return jsonify({"appointments": result1 + result2 + result3, "description": stat})
+
 
 @app.route('/ShowPatientList', methods=['POST'])
 def ShowPatientList():

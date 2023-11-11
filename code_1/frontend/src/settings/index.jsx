c8dc94259fbd9a8b49cd3fb1b71fbaced8bb1728
Zhetai Jiang <z5302785@ad.unsw.edu.au>
2023-11-11 21:20:29 +1100

    w

--- code_1/frontend/src/settings/index.jsx
+++ code_1/frontend/src/settings/index.jsx
@@ -150,46 +150,15 @@ function App({ token }) {
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
     >
-      {
-        isAdmin && (
-          <Form.Item
-            label={
-              <div style={{display: "flex"}}>
-                <div>Time range</div>
-                <QuestionCircleOutlined style={{marginLeft: 2}}/>
-              </div>
-            }
-            name="timeRange"
-            rules={[
-              {
-                required: false,
-              },
-            ]}
-          >
-            <Space>
-              <TimePicker.RangePicker defaultValue={[dayjs(defaultStartTime, format), dayjs(defaultEndTime, format)]} format={format} 
-                picker="time"
-                minuteStep={15}
-                disabledTime={() => ({disabledHours:() => [0,1,2,3,4,5,20,21,22,23]})}
-                onChange={(e, timeString) => onRangeChange(timeString)}
-                value={[dayjs(rangeStartTime, format), dayjs(rangeEndTime, format)]}
-              />
-              <Button style={{marginLeft: 10, backgroundColor: "#C3F3EE", fontWeight:'bold', width: 130}} onClick={clearRangeTime}>Default setting</Button>
-            </Space>
-            
-          </Form.Item>
-        )
-      }
-      
       
       <Form.Item
         label={
           <div style={{display: "flex"}}>
-            <div>Break time</div>
+            <div>Time range</div>
             <QuestionCircleOutlined style={{marginLeft: 2}}/>
           </div>
         }
-        name="breakTime"
+        name="timeRange"
         rules={[
           {
             required: false,
@@ -197,17 +166,49 @@ function App({ token }) {
         ]}
       >
         <Space>
-          <TimePicker.RangePicker defaultValue={[dayjs(dBreakStartTime, format), dayjs(dBreakEndTime, format)]} format={format} 
+          <TimePicker.RangePicker defaultValue={[dayjs(defaultStartTime, format), dayjs(defaultEndTime, format)]} format={format} 
             picker="time"
             minuteStep={15}
             disabledTime={() => ({disabledHours:() => [0,1,2,3,4,5,20,21,22,23]})}
-            onChange={(e, timeString) => onBreakChange(timeString)}
-            value={[dayjs(breakStartTime, format), dayjs(breakEndTime, format)]}
-            // onChange={onRange}
+            onChange={(e, timeString) => onRangeChange(timeString)}
+            value={[dayjs(rangeStartTime, format), dayjs(rangeEndTime, format)]}
           />
-          <Button style={{marginLeft: 10, backgroundColor: "#C3F3EE", fontWeight:'bold', width: 130}} onClick={clearBreakTime}>Default setting</Button>
+          <Button style={{marginLeft: 10, backgroundColor: "#C3F3EE", fontWeight:'bold', width: 130}} onClick={clearRangeTime}>Default setting</Button>
         </Space>
+        
       </Form.Item>
+      
+      
+      {
+        isAdmin && (
+          <Form.Item
+            label={
+              <div style={{display: "flex"}}>
+                <div>Break time</div>
+                <QuestionCircleOutlined style={{marginLeft: 2}}/>
+              </div>
+            }
+            name="breakTime"
+            rules={[
+              {
+                required: false,
+              },
+            ]}
+          >
+            <Space>
+              <TimePicker.RangePicker defaultValue={[dayjs(dBreakStartTime, format), dayjs(dBreakEndTime, format)]} format={format} 
+                picker="time"
+                minuteStep={15}
+                disabledTime={() => ({disabledHours:() => [0,1,2,3,4,5,20,21,22,23]})}
+                onChange={(e, timeString) => onBreakChange(timeString)}
+                value={[dayjs(breakStartTime, format), dayjs(breakEndTime, format)]}
+                // onChange={onRange}
+              />
+              <Button style={{marginLeft: 10, backgroundColor: "#C3F3EE", fontWeight:'bold', width: 130}} onClick={clearBreakTime}>Default setting</Button>
+            </Space>
+          </Form.Item>
+        )
+      }
 
       <br></br>
       <div style={{display: 'flex', justifyContent: 'center'}}>

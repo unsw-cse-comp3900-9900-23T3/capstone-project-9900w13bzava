aa45402db4815e8323179f016759be9b16cc8647
Zhetai Jiang <z5302785@ad.unsw.edu.au>
2023-11-12 21:46:52 +1100

    t

---
+++ code_1/frontend/public/main.js
@@ -0,0 +1,25 @@
+const electron = require('electron');
+const {app} = electron;
+const {BrowserWindow} = electron;
+
+let win;
+ 
+function createWindow() {
+  win = new BrowserWindow({width: 1210, height: 650});
+  win.loadURL(`file://${__dirname}/index.html`);
+  win.on('closed', () => {
+    win = null;
+  });
+}
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+ 
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});

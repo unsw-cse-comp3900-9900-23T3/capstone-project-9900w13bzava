25eaf8240c7f4da7f5d040e5eb48bc49156134d4
Zhetai Jiang <z5302785@ad.unsw.edu.au>
2023-11-13 15:04:48 +1100

    w

--- code_1/frontend/public/main.js
+++ code_1/frontend/public/main.js
@@ -1,14 +1,34 @@
 const electron = require('electron');
 const {app} = electron;
 const {BrowserWindow} = electron;
+// const { spawn } = require('child_process');
+// const path = require('path');
 
-let win;
+let userWindow;
+
+// 启动 Python 后端
+// const backendProcess = spawn('python', [path.join(__dirname, 'python', 'backend.py')]);
+
+// // 捕获 Python 后端的输出
+// backendProcess.stdout.on('data', (data) => {
+//   console.log(`Python Backend Output: ${data}`);
+// });
+
+// // 捕获 Python 后端的错误
+// backendProcess.stderr.on('data', (data) => {
+//   console.error(`Python Backend Error: ${data}`);
+// });
+
+// // 在 Electron 应用关闭时终止 Python 后端
+// app.on('will-quit', () => {
+//   backendProcess.kill();
+// });
  
 function createWindow() {
-  win = new BrowserWindow({width: 1210, height: 650});
-  win.loadURL(`file://${__dirname}/index.html`);
-  win.on('closed', () => {
-    win = null;
+  userWindow = new BrowserWindow({width: 1230, height: 700});
+  userWindow.loadURL(`file://${__dirname}/index.html`);
+  userWindow.on('closed', () => {
+    userWindow = null;
   });
 }
 app.on('ready', createWindow);
@@ -19,7 +39,7 @@ app.on('window-all-closed', () => {
 });
  
 app.on('activate', () => {
-  if (win === null) {
+  if (userWindow === null) {
     createWindow();
   }
 });
